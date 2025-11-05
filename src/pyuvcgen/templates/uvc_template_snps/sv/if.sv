2132d5dfd8b31ce1625e1d1d6adcfae3a565fa74
cirobermudezmarquez <cirofabian.bermudez@gmail.com>
2025-11-05 11:51:19 +0100

    feat: improve interface template and uvc.yaml

--- src/pyuvcgen/templates/uvc_template_snps/sv/if.sv
+++ src/pyuvcgen/templates/uvc_template_snps/sv/if.sv
@@ -8,8 +8,26 @@ interface {{ name }}_if (
   import {{ name }}_pkg::*;
 
   // ================================= INPUTS ================================= //
+  {%- for elem in inputs %}
+  {%- set t = elem.type | default('logic') -%}
+  {%- set w = elem.size | default(1) | int -%}
+  {%- if w == 1 %}
+  {{ "%-12s"|format(t) }} {{ elem.name }};
+  {%- else %}
+  {{ "%-5s"|format(t) }} [{{ "%2s"|format(w-1) }}:0] {{ elem.name }};
+  {%- endif %}
+  {%- endfor %}
 
   // ================================ OUTPUTS ================================= //
+  {%- for elem in outputs %}
+  {%- set t = elem.type | default('logic') -%}
+  {%- set w = elem.size | default(1) | int -%}
+  {%- if w == 1 %}
+  {{ "%-12s"|format(t) }} {{ elem.name }};
+  {%- else %}
+  {{ "%-5s"|format(t) }} [{{ "%2s"|format(w-1) }}:0] {{ elem.name }};
+  {%- endif %}
+  {%- endfor %}
 
   // ============================= INITIAL VALUES ============================= //
 
@@ -21,10 +39,16 @@ interface {{ name }}_if (
   
   clocking cb_drv @(posedge clk_i);
     default input #1ns output #1ns;
+    {%- for elem in inputs %}
+    output {{ elem.name }};
+    {%- endfor %}
   endclocking : cb_drv
 
   clocking cb_drv_neg @(negedge clk_i);
     default input #1ns output #1ns;
+    {%- for elem in inputs %}
+    output {{ elem.name }};
+    {%- endfor %}
   endclocking : cb_drv_neg
 
   clocking cb_mon @(posedge clk_i);
